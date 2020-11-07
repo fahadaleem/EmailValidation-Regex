ce0acf0024e33c179c857f6caa1f3a8e46847002
unknown <55811140+fahadaleem@users.noreply.github.com>
2020-11-07 22:55:42 +0500

    Initial Commit Email Validation

--- Form1.cs
+++ Form1.cs
@@ -27,7 +27,7 @@ namespace WindowsFormsApplication2
         {
 
             string check = textBox1.Text;
-            Regex aa = new Regex("^[a-zA-Z]{1}[a-zA-z0-9]+@(gmail|yahoo|ymail).(co|com)$");
+            Regex aa = new Regex("^[a-zA-Z]{1}[a-zA-z0-9./_-]+@(gmail|yahoo|ymail).(co|com)$");
             bool isValid = aa.IsMatch(check);
 
             label3.Text = isValid ? "Email Validated, it is correct" : "Incorrect Email! Please Write Correct Email";
